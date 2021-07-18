69cfbb873d80cd79d13801837897b146175c8c21
RashidRysaev <rashid.rysaev93@gmail.com>
2021-07-18 10:36:54 +0200

    Project build, userapp created, API created

---
+++ rest_project/userapp/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework.serializers import ModelSerializer
+
+from .models import CustomUser
+
+
+class CustomUserSerializer(ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = (
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "is_superuser",
+        )
