56eb5b1a33d11f6c352e2e2c77d54957aa7895ac
RashidRysaev <rashid.rysaev93@gmail.com>
2021-08-12 23:25:24 +0200

    API version 2.0 build up

--- rest_project/rest_project/settings.py
+++ rest_project/rest_project/settings.py
@@ -72,6 +72,7 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
+    "DEFAULT_VERSIONING_CLASS": "rest_framework.versioning.AcceptHeaderVersioning",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 100
     # "DEFAULT_PARSER_CLASSES": [
--- rest_project/userapp/serializers.py
+++ rest_project/userapp/serializers.py
@@ -6,4 +6,10 @@ from .models import CustomUser
 class CustomUserSerializer(ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ["username", "first_name", "last_name", "email", "is_superuser"]
+        fields = ["username", "first_name", "last_name", "email"]
+
+
+class CustomUserSerializerSuperStuff(ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ["username", "first_name", "last_name", "email", "is_superuser", "is_staff"]
