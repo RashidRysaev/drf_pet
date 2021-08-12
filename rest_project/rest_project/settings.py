db4f9c68c41608d1f914f1406fcbb985309201bb
RashidRysaev <rashid.rysaev93@gmail.com>
2021-08-12 21:32:10 +0200

    An old bug hunted down and got cleaned up

--- rest_project/rest_project/settings.py
+++ rest_project/rest_project/settings.py
@@ -69,7 +69,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        # "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
